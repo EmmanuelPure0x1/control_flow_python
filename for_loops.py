0ffb14f7198aaafa88ad5b705e4502a433919411
EmmanuelPure0x1 <eadejobi@spartaglobal.com>
2020-11-03 16:01:47 +0000

    for loop

---
+++ for_loops.py
@@ -0,0 +1,9 @@
+# Loops for loop and while loop
+# for loop is used to iterate through the data
+# Syntax for variable_name in name_of_data_collection_variable
+
+shopping_list = ["eggs","milk","Supermalt"]
+print(shopping_list)
+
+for items in shopping_list:
+    print("Item:", items)
