8e460874ed212a30f1482768530f619bf7878474
EmmanuelPure0x1 <eadejobi@spartaglobal.com>
2020-11-03 15:54:37 +0000

    int

--- control_flow.py
+++ control_flow.py
@@ -17,13 +17,13 @@ else:
 # 18, 15, U, 12, PG
 # else block should ensure to display message if other conditions do not match
 
-age = input("How old are you? > ")
+age = int(input("How old are you? > "))
 
-if int(age) > 18:
+if age > 18:
     print("You are fine")
-elif int(age) <= 15:
+elif age <= 15:
     print("You will need an adult")
-elif int(age) <= 12:
+elif age <= 12:
     print("You are under age")
 else:
     print("input invalid")
